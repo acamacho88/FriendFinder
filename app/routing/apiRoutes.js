e970346c6cee7aa7d3f0bcc23d923c502d1c0c11
Alex Camacho <alex.c@WalkMe.local>
2018-08-18 13:58:11 -0700

    Added logic to compare all users after submitting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,25 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        friendsList.push(req.body);
-        res.json(true);
+        var newUser = req.body;
+
+        var currBest = friendsList[0];
+        var currBestScore = 0
+
+        for (var i = 0; i < friendsList.length; i++) {
+            var compatScore = 0;
+            var currUser = friendsList[i];
+            for (var j = 0; j < currUser.scores; j++) {
+                compatScore += Math.abs(currUser[j] - req.body.scores[j]);
+            }
+            if (compatScore > currBestScore) {
+                currBest = currUser;
+                currBestScore = compatScore;
+            }
+        }
+
+        // code to show best user in modal
+
+        friendsList.push(newUser);
     });
 };
